cf429999e6ced4d35387861fd20a3dfa7c25564d
jordanandree <jordan.andree@mailchimp.com>
2015-12-14 22:31:29 -0500

    work in progress

---
+++ readme.md
@@ -0,0 +1,8 @@
+# URL Mapper
+
+an experiment w/ python
+
+## Installation
+- Python 2.7 + pip
+- run `pip install -r requirements.txt`
+- run `./script.py`
---
+++ script.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import re
+import pprint
+import sys
+from selenium import webdriver
+
+global driver, dataset
+
+driver  = webdriver.PhantomJS()
+dataset = dict()
+
+def get_page_anchors_for(url, recursive=False):
+  # add a new array for the urls on this page
+  if not url in dataset:
+    dataset[url] = get_anchors_for_page(url)
+
+    # Get the anchors of each new page
+    if recursive:
+      for url in dataset[url]:
+        get_page_anchors_for(url)
+
+def get_anchors_for_page(url):
+  print "Processing anchors for: " + url
+  driver.get(url)
+
+  css = '[role="navigation"] * a[href], [role="main"] * a[href]'
+  anchors = driver.find_elements_by_css_selector(css)
+  urls = []
+
+  for element in anchors:
+    href = element.get_attribute('href')
+
+    # skip if doesn't match *.mailchimp.com pattern
+    pattern = re.compile("http(s?)://(.*\.)?mailchimp\.com(.*)")
+    if not pattern.match(href):
+      continue
+
+    urls.append(href)
+
+  return urls
+
+get_page_anchors_for("http://mailchimp.com", recursive=True)
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(dataset)
+
+driver.quit()
